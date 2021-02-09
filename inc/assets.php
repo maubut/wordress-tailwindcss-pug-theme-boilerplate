f13a723dcda1088de95e604c02c8492bb9da5acb
Mikhael Aubut <mikhaelaubut@gmail.com>
2021-02-09 08:42:04 -0500

    fix: pug suport

--- inc/assets.php
+++ inc/assets.php
@@ -2,7 +2,7 @@
 add_action('wp_enqueue_scripts', function () {
   wp_deregister_script('jquery');
 
-  wp_enqueue_style('theme-css', get_template_directory_uri() . "/build/css/frontend.css",  false, null);
+  wp_enqueue_style('theme-css', get_template_directory_uri() . "/build/css/app.css",  false, null);
 
   wp_enqueue_script('theme-js', get_template_directory_uri() . "/build/js/app.js", [], null);
 
