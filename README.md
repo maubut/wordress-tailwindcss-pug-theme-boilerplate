4a7c595ecca17155f9a281559d837342cf97dd35
Mikhael Aubut <mikhaelaubut@gmail.com>
2021-02-09 08:55:21 -0500

    feat: add sample template

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - https://github.com/mishterk/wp-tailwindcss-theme-boilerplate
 - https://nystudio107.com/blog/speeding-up-tailwind-css-builds
 - https://medium.com/@yazeedobaid/postcss-in-laravel-mix-bc433bfcf7a
+- https://github.com/parfaitementweb/tailwind-mix-starter/blob/master/webpack.mix.js
---
+++ tpl-sample.php
@@ -0,0 +1,19 @@
+
+<?php
+/* Template Name: Sample */
+$base = include('inc/theme-data.php');
+
+$data = array_merge( $base, [
+	'post' => [
+		'title' => get_the_title(),
+    'content' => apply_filters( 'the_content', $post->post_content ),
+  ],
+] );
+
+
+if ( function_exists ( 'output' ) ) {
+  output( basename(__FILE__, '.php') , $data );
+}
+?>
+
+
